7c60f5c42321a565df5f356db28365026ab7ca90
Naveen Kumar <naveen.kumar@avrioc.com>
2025-03-13 13:51:03 +0400

    new test

--- testing.ps1
+++
@@ -1,6 +0,0 @@
-try {
-    $PublicIP = Invoke-RestMethod -Uri "https://api64.ipify.org?format=text"
-    Write-Output "Your Public IP Address is: $PublicIP"
-} catch {
-    Write-Output "Failed to retrieve public IP. Check your internet connection."
-}
